d110e170837fbfc88f671900b729d2a1341bf2ed
doueov <gitpdyeon@gmail.com>
2024-08-29 11:00:28 +0900

    직접 데이터베이스, 훅, fetch 작업 가능

---
+++ src/app/components/UserListComponent.js
@@ -0,0 +1,13 @@
+import database from "@/app/lib/database"
+
+export default async function UserListComponent() {
+    const users = await database.getUsers()
+    return <div>
+        <h1>Users</h1>
+        <ul>
+            {
+                users.map(u => <li>{u.id}</li>)
+            }
+        </ul>
+    </div>
+}
