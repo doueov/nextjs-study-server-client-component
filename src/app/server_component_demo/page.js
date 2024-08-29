9eb9130551b427dab8d46f113d7f810ec9684a9a
doueov <gitpdyeon@gmail.com>
2024-08-29 11:01:59 +0900

    page 수정

--- src/app/server_component_demo/page.js
+++ src/app/server_component_demo/page.js
@@ -1,10 +1,11 @@
-import MyServerComponent from "@/app/components/MyServerComponent"
+import MyServerComponent from "@/components/MyServerComponent"
+import UserListComponent from "@/components/UserListComponent"
 
 export default async function ServerComponentDemoPage() {
     console.log("from Server Component Demo Page")
     return <div>
         <h1>Server Component Demo Page</h1>
-        {/* 서버 컴포넌트 렌더링 */}
         <MyServerComponent />
+        <UserListComponent />
     </div>
 }
